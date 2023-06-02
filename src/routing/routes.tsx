88fb767ff89c06c2a6fd3408862da07a8ed9020c
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-06-02 11:13:16 +0200

    Handling Errors

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,14 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
-import UserListPage from "./UserList";
 import UserDetail from "./UserDetail";
 import Layout from "./Layout";
 import UsersPage from "./UsersPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
